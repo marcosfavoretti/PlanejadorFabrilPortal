659050af7c1da3c2e836cba0e6c1afab7503d2ed
marcosfavoretti <marcosfavorettijunior@gmail.com>
2026-05-28 11:21:07 -0300

    chore: adiciona cliente de auditoria gerado

---
+++ src/api/audit/index.ts
@@ -0,0 +1,17 @@
+export type {
+  AuditLogControllerCreate202,
+  AuditLogControllerCreateMutation,
+  AuditLogControllerCreateMutationRequest,
+  AuditLogControllerCreateMutationResponse,
+} from './models/AuditLogControllerCreate';
+export type {
+  AuditLogControllerFindAll200,
+  AuditLogControllerFindAllQuery,
+  AuditLogControllerFindAllQueryParams,
+  AuditLogControllerFindAllQueryResponse,
+} from './models/AuditLogControllerFindAll';
+export type { AuditLogDto } from './models/AuditLogDto';
+export type { AuditLogResponseDto } from './models/AuditLogResponseDto';
+export type { PaginatedAuditLogResponseDtoDto } from './models/PaginatedAuditLogResponseDtoDto';
+export { auditLogControllerCreate } from './client/auditLogControllerCreate';
+export { auditLogControllerFindAll } from './client/auditLogControllerFindAll';
